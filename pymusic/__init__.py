be3e0e422c46e9bce55267ede8ad7d4a49a84661
Ransford Oppong <faradayfoundationgh@gmail.com>
2026-04-05 12:34:02 +0000

    fix: switch to static-ffmpeg (bundles ffmpeg+ffprobe)

--- pymusic/__init__.py
+++ pymusic/__init__.py
@@ -23,7 +23,7 @@ from pymusic.exceptions import (
     FormatError,
 )
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "pyMusic Contributors"
 __license__ = "MIT"
 __package_name__ = "pymusic-downloader"
